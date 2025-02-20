5114f13f557d25218253691df89a5b329600bd61
Rafael de Oliveira Carvalho <rdeoliveiracarvalhoo@gmail.com>
2025-02-20 18:06:42 -0300

    atualizando versão jogo do numero secreto

--- 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
+++ 3411-praticando-logica-projeto_inicial/sorteador-numeros/app.js
@@ -1,85 +1,56 @@
-// function sortear(){
-//     let quantidade = parseInt(document.getElementById('quantidade').value);
-//     let de = parseInt(document.getElementById('de').value);
-//     let ate = parseInt(document.getElementById('ate').value);
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let de = parseInt(document.getElementById('de').value);
+    let ate = parseInt(document.getElementById('ate').value);
 
-//     let numero;
-//     let sorteados = [];
+    let sorteados = [];  
 
-//     if (de >= ate || quantidade > (ate - de + 1)){
-//         alert('Tente novamente e insira com valores do "De" menores que "Até" ou com valores para "Quantidade de número"');
-//         reiniciar();
-//         alterarStatusBotao();
-//         return; 
-        
-//     }
-//     else{
-        
+    if(quantidade < 1 || quantidade > (ate - de + 1) || de >= ate ){
+        alert('Por favor, insira valores válidos para gerar o número aleatório.')
+        alterarStatusBotao();
+        reiniciar();
+        return;
     
+    }else{
 
-//     for(let i = 0; i < quantidade;i++){
-//         numero = obterNumeroAleatorio(de, ate);
+    for (let i = 0; i < quantidade; i++) {
+        let numero = obterNumeroAleatorio(de, ate);
         
-//         while(sorteados.includes(numero)){
-//             numero = obterNumeroAleatorio(de, ate);
-            
-//         }
-//          sorteados.push(numero);
-//     }
-// }
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
 
-//     let resultado = document.getElementById('resultado');
-//     resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
-//     alterarStatusBotao();
-// }
+        sorteados.push(numero);
+    }
 
-// function obterNumeroAleatorio(min, max){
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// function alterarStatusBotao(){
-//     let botao = document.getElementById('btn-reiniciar');
-//     if(botao.classList.contains('container__botao-desabilitado')){
-//         botao.classList.remove('container__botao-desabilitado');
-//         botao.classList.add ('container__botao');
-//     } else {
-//         botao.classList.remove('container__botao');
-//         botao.classList.add('container__botao-desabilitado');
-//     }
-// }
-
-// function reiniciar(){
-//     document.getElementById('quantidade').value = '';
-//     document.getElementById('de').value = '';
-//     document.getElementById('ate').value = '';
-//     document.getElementById('resultado').innerHTML = ' <label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
-//     alterarStatusBotao();
-// }
-
-function sortear(){
-    let quantidade =  parseInt(document.getElementById('quantidade').value);
-    let de =  parseInt(document.getElementById('de').value);
-    let ate =  parseInt(document.getElementById('ate').value);
-   
-    let sorteados = [];
-    let numero;
-    
-   
-
+    let resultado = document.getElementById("resultado"); 
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    alterarStatusBotao();
+}
 }
 
-    let resultado = document.getElementById('resultado');
-    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`
-
-
-function obterNumeroAleatorio(min, max){
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function obterNumeroAleatorio(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-     
-        
+function alterarStatusBotao(){
+    let botao = document.getElementById("btn-reiniciar");
     
-        
+    if(botao.classList.contains('container__botao-desabilitado')){ // se contiver sattus da classe desabilitado
+     botao.classList.remove('container__botao-desabilitado'); // remove a classe desabilitado
+     botao.classList.add('container__botao'); //adiciona a classe ativa 
     
-        
-    
+    } else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML =  '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>'
+    alterarStatusBotao();
+
+}
